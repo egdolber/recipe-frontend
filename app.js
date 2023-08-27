3549f86077e42d2f2a145600b18b5e09239f12cf
Edward Dolber-Smith <ed.dolbersmith@gmail.com>
2023-08-27 14:08:31 -0700

    view home page generated by pug templates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// external modules
 const express = require('express');
 const dotenv = require('dotenv').config();
-const { dirname, join } = require('path');
+const path = require('path');
 const { fileURLToPath } = require('url');
 const colors = require('colors');
 
@@ -8,15 +9,21 @@ console.log(`============= Client Interface ++==================`.white);
 console.log(`Project Name: ${process.env.PROJECT}`.blue);
 const port = process.env.PORT || 3001;
 
+// app varialbes
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
+// routes defintions
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // res.status(200).send('Hello World!');
+  res.render('index', { Title: 'Home' });
 });
 
+// server activation
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`.blue);
 });
