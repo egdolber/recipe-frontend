f83aec4470a25783cd105226e27c5b360f6851e2
Edward Dolber-Smith <ed.dolbersmith@gmail.com>
2023-08-26 22:32:44 -0700

    add hello world

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ console.log(`Project Name: ${process.env.PROJECT}`.blue);
 const port = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`.blue);
